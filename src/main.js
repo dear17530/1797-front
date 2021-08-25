c37171b809beb6a5f2780347e23f1040d39076e6
dear17530 <dear17530@gmail.com>
2021-08-25 15:20:41 +0800

    update

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
-import ImgInputer from 'vue-img-inputer'
 import 'vue-img-inputer/dist/index.css'
 import mixin from './mixin.js'
 import '@egjs/vue-flicking/dist/flicking.css'
@@ -23,9 +22,7 @@ import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 import VuePaycard from 'vue-paycard'
-import { VueMasonryPlugin } from 'vue-masonry'
 import VueApexCharts from 'vue-apexcharts'
-import Textra from 'vue-textra'
 import Flicking from '@egjs/vue-flicking'
 
 import VueGtag from 'vue-gtag'
@@ -39,15 +36,12 @@ const FilePond = vueFilePond(
 )
 
 axios.defaults.baseURL = process.env.VUE_APP_API
-Vue.use(Textra)
 Vue.use(VueAxios, axios)
 Vue.use(VueSweetalert2)
-Vue.component('ImgInputer', ImgInputer)
 Vue.use(Flicking)
 Vue.component('apexchart', VueApexCharts)
 Vue.component('vueFilePond', FilePond)
 Vue.use(VuePaycard)
-Vue.use(VueMasonryPlugin)
 Vue.mixin(mixin)
 
 Vue.config.productionTip = false
